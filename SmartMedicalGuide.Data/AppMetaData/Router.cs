fd6f34aa154d2076846a727dcf734d8576d2f36e
HusamSM <hossam777893147@gmail.com>
2026-04-16 20:50:53 +0300

    add router

--- SmartMedicalGuide.Data/AppMetaData/Router.cs
+++ SmartMedicalGuide.Data/AppMetaData/Router.cs
@@ -8,81 +8,39 @@
         public const string version = "V1";
         public const string Rule = root + "/" + version + "/";
 
-        public static class RoleRouting
+        public static class AppointmentHistoryRouting
         {
-            public const string Prefix = Rule + "Role/";
-            public const string List = Prefix + "List";
-            public const string GetByID = Prefix + SingleRoute;
-            public const string Create = Prefix + "Create";
-
-        }
-        public static class PatientRouting
-        {
-            public const string Prefix = Rule + "Patient/";
+            public const string Prefix = Rule + "AppointmentHistory/";
             public const string List = Prefix + "List";
             public const string GetByID = Prefix + SingleRoute;
             public const string Create = Prefix + "Create";
             public const string Edit = Prefix + "Edit";
             public const string Delete = Prefix + "Delete";
+
         }
-        public static class DoctorRouting
+        public static class AttachmentRouting
         {
-            public const string Prefix = Rule + "Doctor/";
+            public const string Prefix = Rule + "Attachment/";
             public const string List = Prefix + "List";
             public const string GetByID = Prefix + SingleRoute;
-            public const string GetByName = Prefix + NameRoute;
             public const string Create = Prefix + "Create";
             public const string Edit = Prefix + "Edit";
             public const string Delete = Prefix + "Delete";
 
         }
-        public static class SystemSetting
+        public static class AuditLogRouting
         {
-            public const string Prefix = Rule + "SystemSetting/";
+            public const string Prefix = Rule + "AuditLog/";
             public const string List = Prefix + "List";
             public const string GetByID = Prefix + SingleRoute;
-            public const string GetByName = Prefix + NameRoute;
             public const string Create = Prefix + "Create";
             public const string Edit = Prefix + "Edit";
             public const string Delete = Prefix + "Delete";
 
         }
-        public static class SpecializationRouting
+        public static class ChatRouting
         {
-            public const string Prefix = Rule + "Specialization/";
-            public const string List = Prefix + "List";
-            public const string GetByID = Prefix + SingleRoute;
-            public const string GetByName = Prefix + NameRoute;
-            public const string Create = Prefix + "Create";
-            public const string Edit = Prefix + "Edit";
-            public const string Delete = Prefix + "Delete";
-
-        }
-        public static class ReportRouting
-        {
-            public const string Prefix = Rule + "Specialization/";
-            public const string List = Prefix + "List";
-            public const string GetByID = Prefix + SingleRoute;
-            public const string GetByName = Prefix + NameRoute;
-            public const string Create = Prefix + "Create";
-            public const string Edit = Prefix + "Edit";
-            public const string Delete = Prefix + "Delete";
-
-        }
-        public static class FavoriteRouting
-        {
-            public const string Prefix = Rule + "Favorite/";
-            public const string List = Prefix + "List";
-            public const string GetByID = Prefix + SingleRoute;
-            public const string GetByName = Prefix + NameRoute;
-            public const string Create = Prefix + "Create";
-            public const string Edit = Prefix + "Edit";
-            public const string Delete = Prefix + "Delete";
-
-        }
-        public static class LabRouting
-        {
-            public const string Prefix = Rule + "Lab/";
+            public const string Prefix = Rule + "Chat/";
             public const string List = Prefix + "List";
             public const string GetByID = Prefix + SingleRoute;
             public const string Create = Prefix + "Create";
@@ -101,9 +59,50 @@
             public const string Delete = Prefix + "Delete";
 
         }
+        public static class DoctorRouting
+        {
+            public const string Prefix = Rule + "Doctor/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string GetByName = Prefix + NameRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
         public static class DoctorAppointmentRouting
         {
             public const string Prefix = Rule + "DoctorAppointment/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class DoctorScheduleRouting
+        {
+            public const string Prefix = Rule + "DoctorSchedule/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class FavoriteRouting
+        {
+            public const string Prefix = Rule + "Favorite/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class LabRouting
+        {
+            public const string Prefix = Rule + "Lab/";
             public const string List = Prefix + "List";
             public const string GetByID = Prefix + SingleRoute;
             public const string Create = Prefix + "Create";
@@ -121,6 +120,202 @@
             public const string Delete = Prefix + "Delete";
 
         }
+        public static class LabServiceRouting
+        {
+            public const string Prefix = Rule + "LabService/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class MedicalReportRouting
+        {
+            public const string Prefix = Rule + "MedicalReport/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class MessageRouting
+        {
+            public const string Prefix = Rule + "Message/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class NotificationRouting
+        {
+            public const string Prefix = Rule + "Notification/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class PatientRouting
+        {
+            public const string Prefix = Rule + "Patient/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+        }
+        public static class PaymentRouting
+        {
+            public const string Prefix = Rule + "Payment/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+        }
+        public static class PrescriptionRouting
+        {
+            public const string Prefix = Rule + "Prescription/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class PrescriptionItemRouting
+        {
+            public const string Prefix = Rule + "PrescriptionItem/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class ReportRouting
+        {
+            public const string Prefix = Rule + "Specialization/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string GetByName = Prefix + NameRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class ReviewRouting
+        {
+            public const string Prefix = Rule + "Review/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+
+
+        public static class RoleRouting
+        {
+            public const string Prefix = Rule + "Role/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+
+        }
+        public static class SearchHistoryRouting
+        {
+            public const string Prefix = Rule + "SearchHistory/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class SpecializationRouting
+        {
+            public const string Prefix = Rule + "Specialization/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string GetByName = Prefix + NameRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+
+
+        public static class SystemSetting
+        {
+            public const string Prefix = Rule + "SystemSetting/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string GetByName = Prefix + NameRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class TransactionRouting
+        {
+            public const string Prefix = Rule + "Transaction/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class UserRefreshTokenRouting
+        {
+            public const string Prefix = Rule + "UserRefreshToken/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class UserSessionRouting
+        {
+            public const string Prefix = Rule + "UserSession/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class VerificationRequestRouting
+        {
+            public const string Prefix = Rule + "VerificationRequest/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+        public static class WalletRouting
+        {
+            public const string Prefix = Rule + "Wallet/";
+            public const string List = Prefix + "List";
+            public const string GetByID = Prefix + SingleRoute;
+            public const string Create = Prefix + "Create";
+            public const string Edit = Prefix + "Edit";
+            public const string Delete = Prefix + "Delete";
+
+        }
+
+
+
 
         public static class UserRouting
         {
@@ -132,15 +327,7 @@
             public const string Delete = Prefix + "Delete";
 
         }
-        public static class PaymentRouting
-        {
-            public const string Prefix = Rule + "Payment/";
-            public const string List = Prefix + "List";
-            public const string GetByID = Prefix + SingleRoute;
-            public const string Create = Prefix + "Create";
-            public const string Edit = Prefix + "Edit";
-            public const string Delete = Prefix + "Delete";
-        }
+        
 
     }
 }
