70a2c4544073c3a0c7a9900d5717a81554b979d4
HusamSM <hossam777893147@gmail.com>
2026-04-16 18:06:34 +0300

    at folders

---
+++ SmartMedicalGuide.Core/Features/Attachments/Queries/Results/GetSingleAttachmentResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMedicalGuide.Core.Features.Attachments.Queries.Results
+{
+    internal class GetSingleAttachmentResponse
+    {
+    }
+}
---
+++ SmartMedicalGuide.Core/Features/UserSession/Queries/Handlers/UserSessionQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMedicalGuide.Core.Features.UserSession.Queries.Handlers
+{
+    internal class UserSessionQueryHandler
+    {
+    }
+}
